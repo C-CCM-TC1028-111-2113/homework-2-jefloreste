f4d0e4ff231dfbd1ad8746ccff5c59a264080fa2
jefloreste <71605758+jefloreste@users.noreply.github.com>
2021-08-26 00:08:12 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,7 +1,26 @@
 def main():
-    # Escribe el código adecuado para completar el programa
-    pass
+    return()
 
+a = int(input("Insert a number: "))
+b = int(input("Insert a number: "))
+c = int(input("Insert a number: "))
 
-if __name__=='__main__':
-    main()
+if a<b and a<c:
+    if b<c:
+        first, second, third = a,b,c
+    else:
+        first, second, third = a,c,b
+elif b<a and b<c:
+    if a<c:
+        first, second, third = b,a,c
+    else:
+        first, second, third = b,c,a
+else:
+    if a<b:
+        first, second, third = c,a,b
+    else:
+        first, second, third = c,b,a
+
+print("First: ", first)
+print("Second: ", second)
+print("Third: ", third)
