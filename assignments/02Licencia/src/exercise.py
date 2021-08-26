43c481400ae29507b0ac0b15d985c3caa316700d
jefloreste <71605758+jefloreste@users.noreply.github.com>
2021-08-25 23:44:26 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,18 @@
-
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    return()
 
+age = int(input("Insert your age: "))
 
-if __name__ == '__main__':
-    main()
+if age >= 18:
+   
+   legal_license = input("Do you have a license? (Y/N): ")
+   
+   if legal_license == "Y":
+    print("You're eligible for a license")
+   elif legal_license == "N":
+    print("You're not eligible for a license")
+   else:
+    print("Incorrect information. Please try again.")
+
+elif age < 18:
+   print("You're not eligible for a license")
